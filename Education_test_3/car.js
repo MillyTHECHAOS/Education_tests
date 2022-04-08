077edd9c1604faef08240bc0ad873a812b9cc6b8
MillyTHECHAOS <101138626+MillyTHECHAOS@users.noreply.github.com>
2022-04-08 16:06:55 +0300

    Add files via upload

---
+++ Education_test_3/car.js
@@ -0,0 +1,17 @@
+const carName="Eve"
+let maxCarSpeed=200
+let carOwner="John"
+
+maxCarSpeed=190
+carOwner="Kelly"
+console.log("У машины изменилась скорость", maxCarSpeed)
+console.log("У машины изменился владелец", carOwner)
+
+maxCarSpeed=180
+carOwner="Elly"
+console.log("У машины опять изменилась скорость", maxCarSpeed)
+console.log("У машины опять изменился владелец", carOwner)
+
+alert(carName)
+alert(maxCarSpeed)
+alert(carOwner)
---
+++ Education_test_3/index.js
@@ -0,0 +1,17 @@
+// JavaScript код с созданием переменных
+
+/* Автор:
+MillyTHECHAOS */
+
+let myName="Ruslan"
+let mySurname="Panov"
+let myFavoriteDrink="Coca-Cola"
+let myFavoriteAnimal="Shark"
+let myFavoriteProgrammingLanguage="JavaScript"
+
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
+
